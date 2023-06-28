547e9afc0c236d889ff688cd5315b03d5b658411
Dicky Nih Boss <dickyassidiq45@gmail.com>
2023-06-28 09:33:50 +0700

    react init & react router

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Profile = () => {
+  return (
+    <div>
+      Profile
+      <div>
+        <Link to="/">link ke halaman Dashboard</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
